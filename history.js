a0a88e5c1e712330f343f53858eeffe720f7aef7
arvgta <arvgta@gmail.com>
2013-01-30 00:03:48 +0100

    Update history.js

--- history.js
+++ history.js
@@ -11,57 +11,64 @@
     // The History class
     var History = function($this, options) { var
       //Private 
-        //Getters
-        gId = function() { return $this1.attr('id'); },
-        gHId = function() { return '#' + gId(); },
-        gD = function() { return $(gHId()); },
-        gS1 = function() { return settings['div1']; },
-        gDiv = function() { return div = gS1() ? $(gS1()) : gD(); },
-        gFn = function() { return fn = settings['fn']; },
-
-        //Local variables
         settings = $.extend({
             'div1' : 'body div:first',
-            'fn' : null,
             'verbosity'    : 0,
             'completedEventName' : 'statechangecomplete',
             'scripts' : true,
             'cb' : null
-        }, options),
-			
-        $this1 = $this,
-        div = gDiv(), 
-        fn = gFn(),
+        }, options),        
+        
+        //Getters
+        gId = function() { return $thisId1; },
+        gD = function() { return $(gId()); },
+        gS1 = function() { return settings['div1']; },
+
+        //Local variables
+        $thisId1 = gS1(), 
         scriptsd = settings['scripts'],
         cb = settings['cb'],		
         bHistory = window.History, 
         $data, $scripts, $scriptsO,
-   
+        
         //Helper functions
         documentHtml = function(html) {
-            var $r, result = String(html)
+            var result = String(html)
                 .replace(/<\!DOCTYPE[^>]*>/i, '')
                 .replace(/<(html|head|body|title|meta|script|link)([\s\>])/gi,
                     '<div class="document-$1"$2')
                  .replace(/<\/(html|head|body|title|meta|script|link)\>/gi,'</div>')
             ;
 
-            if(result.trim().substr(0,1) == '<') $r = $(result);
-            else $r = $('<div><pre>' + result + '</pre></div>');
-            
-            return $r;
+            return $(result);
         },
 		
-        hello = function() { log('Entering History, div : ' + (gS1() ?  gS1() : gHId()), 0); },
-        gData = function(h) { $data = documentHtml(h); return $data; },
-        detScripts = function() { var d = $data.find('.document-script, .document-link'); 
-             return $scripts = (d.length ? d.detach() : d); },
+        hello = function() { log('Entering History, div : ' + gId(), 0); },
+        
+        detScripts = function() { 
+            var d = $data.find('.document-script, .document-link'); 
+            $scripts = (d.length ? d.detach() : d); 
+        },
+        
+        _callback = function(f) { 
+            $this; if(cb) cb(); 
+            if(f) $(window).trigger(settings['completedEventName']);
+        }, 
 
         log = function(m, v) { if(!v) v = 1; settings['verbosity'] >= v && window.console && console.log(m); },
         informGA = function() { typeof window._gaq !== 'undefined'  && window._gaq.push(['_trackPageview', bHistory.getState().url.replace(bHistory.getRootUrl(), '')]); },
-        //deepLink = function(h) { return h && h.substring(h.lastIndexOf('/') + 1).indexOf('.') != -1; },
-        cacheScripts = function() { $scriptsO = $scripts; },
-        setupClicks = function() { log('setupClicks()'); div.find('a').each(function() { parseLink(this); }); log('setupClicks succeeded!'); },
+
+        setupClicks = function(h) { 
+            log('setupClicks() on div ID : ' + gId()); 
+            if(h) gD().replaceWith(h); //If HTML is passed replace div
+            
+            //Re-ajaxify content div
+            gD().find('a').each(function() { 
+                parseLink(this); }
+            ); 
+            
+            log('setupClicks succeeded!'); 
+        },
 		
         parseLink = function(l) { log('parseLink(\'' + l.href + '\')'); 
             if($.isUrlInternal(l.href) && !$(l).find('.no-ajaxy').length) 
@@ -80,7 +87,9 @@
 		
         findScriptText = function(t) { var r = false;
             $scriptsO.each(function(){ var $s = $(this); 
-                if($s.text() == t || $s.attr('src') == t || $s.attr('href') == t ) r = true; });
+                if($s.text() == t || $s.attr('src') == t || $s.attr('href') == t ) r = true; 
+            });
+            
             return r;
         },
 		
@@ -89,19 +98,19 @@
                 var $script = $(this), scriptText = $script.text(), scriptHref = $script.attr('href'), scriptSrc = $script.attr('src'), scriptNode = document.createElement('script');
                              
                 if(scriptHref) { 
-                     if(!scriptsd || !findScriptText(scriptHref)) { log('link detected');
+                     if(!scriptsd || !findScriptText(scriptHref)) { log('CSS : ' + scriptHref, 2);
                          $('head link').last().after('<link rel="stylesheet" type="text/css" href="' + scriptHref + '" />');
                      }
                 }
                 else {
                     if(scriptSrc) {
-                        if(!scriptsd || !findScriptText(scriptSrc)) { log('script src detected');
+                        if(!scriptsd || !findScriptText(scriptSrc)) { log('script src : ' + scriptSrc, 2);
                             scriptNode.src = scriptSrc;
                             contentNode.appendChild(scriptNode);
                         }
                     } 
                     else {
-                        if(!scriptsd || !findScriptText(scriptText)) { log('inline script detected');
+                        if(!scriptsd || !findScriptText(scriptText)) { log('inline script : ' + scriptText, 2);
                             scriptNode.appendChild(document.createTextNode(scriptText));
                             contentNode.appendChild(scriptNode);
                         }
@@ -109,88 +118,37 @@
                 }
             });
         },
-		
-        lFn = function() { if(fn) { fn(div); log('lFn div id: ' + div.attr('id'), 2); } },
-        allDivs = function(h) { var f = false;
+        
+        handleScripts = function(f) {
+             detScripts();
+             if(f) addScripts();
+             $scriptsO = $scripts;
+        },
+        
+        allDivs = function(h) {
             $this.each(function(){ 
-                  $this1 = $(this); 
-                  div = gD(); 
-                  f = h();
-            } ); 
-            
-            if(!f) { //no content div in target -> load in entire data, as we're assuming deep-link
-               gD().html($data.html());
-               div = gD();
-               lFn();
-				
-               //Re-ajaxify content div
-               setupClicks();
-            }
+                  $thisId1 = '#' + $(this).attr('id'); 
+                  h();
+            }); 
         },
         
-        fnDiv1 = function() { lFn(); return true; },
-		
-        __construct = function() { $(function () { //on DOMready
-            hello();
-            
-            if(!bHistory.enabled) return;
-            
-            if(gS1()) {
-                 fnDiv1();
-            }                 
-            else allDivs(fnDiv1);
-
-            $.ajax({
-                url: location,
-                success: function(h) {
-                    $data = documentHtml(h);
-                    detScripts();
-                    cacheScripts();
-                    if(gS1) setupClicks(); 
-                    else allDivs(setupClicks);
-					
-                    $this; if(cb) cb();
-                }
-            });
-            
-            }); //end on DOMready
-        },
-		
         fnDiv = function() { //Replace div
-            var $divhtml = $data.find(gHId());
-            
-            if(!$divhtml.length) return false;
-            
-            gD().replaceWith($divhtml);
-            div = gD();
-            lFn();
-				
-            //Re-ajaxify div
-            setupClicks();
-            
-            return true;
+            setupClicks($data.find(gId()));
         },
 		
-        cDiv = function(h) { //Replace all divs
-            gData(h);
-            allDivs(fnDiv);			 
+        cDivs = function(h, f) { //Replace all divs
+            $data = documentHtml(h);
+            if(f) allDivs(fnDiv);
+            handleScripts(f);            
  
-            updateTitle();
-                
-            detScripts(); 
-            addScripts();
-            cacheScripts();				
-			
-            informGA();        
-            
-            $this; if(cb) cb();			
-                
-            $(window).trigger(settings['completedEventName']);
-			
-        },
-        
-        check_content_type = function(c){
-            return c.indexOf('text') != -1 || c.indexOf('javascript') != -1;
+            if(!f) { 
+                if(gS1()) setupClicks(); 
+                else allDivs(setupClicks);              
+            } else {  
+                updateTitle(); informGA();
+            }
+                      
+            _callback(f);		
         },
         
         stateChange = function(){
@@ -201,28 +159,39 @@
             var xhr = $.ajax({
                 url: href,
                 success: function(h) { 
-                    if(check_content_type(xhr.getResponseHeader("Content-Type"))) cDiv(h);
-                    else location = href;                    
+                    if(xhr.getResponseHeader("Content-Type").indexOf('text/html') != -1) cDivs(h, 1);
+                    else location = href; //not HTML                  
                 }
             });
         };
 		
-        // Hook into State Changes
-        window.onstatechange = stateChange;
- 
-    // Run constructor
-    __construct();
+        // Run constructor
+        $(function () { //on DOMready
+            hello();
+            
+            if(!bHistory.enabled) return;
+           
+            // Hook into State Changes
+            window.onstatechange = stateChange;
+        
+            $.ajax({
+                url: location,
+                success: function(h) { cDivs(h); }
+            });
+            
+        }); //end on DOMready
 	
-}; //end History class
+    }; //end History class
 
     // Register jQuery function
     $.fn.history = function(options) {
         var $this = $(this),
             _init = function() { new History($this, options); },
-            init0 = function() { $.getScript('http://4nf.org/js/bhistory.js', _init)}; 
-		
-        $.getScript('http://4nf.org/js/urlinternal.js', init0);
-		
+            init0 = function() { $.getScript('http://4nf.org/js/bhistory.js', _init)},
+            init1 = function() { $.getScript('http://4nf.org/js/urlinternal.js', init0)}; 
+		 
+            init1();
+        
         return $this;
     };
 })(jQuery);
