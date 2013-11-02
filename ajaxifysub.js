1fc479fd238b14112f6abbbc8a353cd7bf538cd7
Arvind Gupta <arvgta@gmail.com>
2013-11-02 16:22:06 +0100

    Update ajaxifysub.js

--- ajaxifysub.js
+++ ajaxifysub.js
@@ -3,3 +3,4 @@ log | con = window.console | { verbosity: 0 } | (m) l < verbosity && con && con.
 isHtml | (x) d=x.getResponseHeader("Content-Type"); r=d&&(d.indexOf("text/html")+1||d.indexOf("text/xml")+1)
 replD | (h) r=String(h).replace(docType, "").replace(tagso, div12).replace(tagsc,"</div>")
 _parseHTML | (h) r=$.trim($.replD(h)
+pages | (h) string : for(var i=0; i<d.length; i++) if(d[i][0]==h) r=d[i][1]; object : d.push(h);
