bfd86502bbc1b825ce62fe0e2bb0472762003236
Arvind Gupta <arvgta@gmail.com>
2013-11-02 21:04:35 +0100

    Update ajaxifysub.js

--- ajaxifysub.js
+++ ajaxifysub.js
@@ -1,4 +1,4 @@
-all | (t, fn) $this.each( f{ t = t.split("*").join("$(this)"); t += ";"; eval(t); })
-log | con = window.console | { verbosity: 0 } | (m) l < verbosity && con && con.log(m)
-isHtml | (x) ®(d=x.getResponseHeader("Content-Type")), d&&(d.indexOf("text/html")+1||d.indexOf("text/xml")+1)
-replD | (h) ®String(h).replace(docType, "").replace(tagso, div12).replace(tagsc,"</div>")
+all(t, fn) $this.each( f{ t = t.split("*").join("$(this)"); t += ";"; eval(t); })
+log(m) | con = window.console | { verbosity: 0 } | l < verbosity && con && con.log(m)
+isHtml(x) ®(d=x.getResponseHeader("Content-Type")), d&&(d.indexOf("text/html")+1||d.indexOf("text/xml")+1)
+replD(h) ®String(h).replace(docType, "").replace(tagso, div12).replace(tagsc,"</div>")
