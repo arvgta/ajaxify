cac7e4ffd4e7a3674d18f453a698db894e1eafa3
Arvind Gupta <arvgta@gmail.com>
2013-11-03 15:08:52 +0100

    Update ajaxifysub.js

--- ajaxifysub.js
+++ ajaxifysub.js
@@ -1,5 +1,4 @@
 addAll(PK) ®addAll.replace(/PK/g, PK)
 all(t, fn) $this.each( f{ t = t.split("*").join("$(this)"); t += ";"; eval(t); })
-log(m) | con = window.console | { verbosity: 0 } | l < verbosity && con && con.log(m)
 isHtml(x) ®(d=x.getResponseHeader("Content-Type")), d&&(d.indexOf("text/html")+1||d.indexOf("text/xml")+1)
 replD(h) ®String(h).replace(docType, "").replace(tagso, div12).replace(tagsc,"</div>")
