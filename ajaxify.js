50ed7d57462674e38f50faf4d532a0b0e80730d5
Arvind Gupta <arvgta@gmail.com>
2013-11-30 15:44:56 +0100

    Issue #12 lifted

--- ajaxify.js
+++ ajaxify.js
@@ -525,7 +525,7 @@ function _initAjaxify(s) {
     };
 })(jQuery);﻿(function(e){e.fn.hoverIntent=function(t,n,r){var i={interval:100,sensitivity:7,timeout:0};if(typeof t==="object"){i=e.extend(i,t)}else if(e.isFunction(n)){i=e.extend(i,{over:t,out:n,selector:r})}else{i=e.extend(i,{over:t,out:t,selector:n})}var s,o,u,a;var f=function(e){s=e.pageX;o=e.pageY};var l=function(t,n){n.hoverIntent_t=clearTimeout(n.hoverIntent_t);if(Math.abs(u-s)+Math.abs(a-o)<i.sensitivity){e(n).off("mousemove.hoverIntent",f);n.hoverIntent_s=1;return i.over.apply(n,[t])}else{u=s;a=o;n.hoverIntent_t=setTimeout(function(){l(t,n)},i.interval)}};var c=function(e,t){t.hoverIntent_t=clearTimeout(t.hoverIntent_t);t.hoverIntent_s=0;return i.out.apply(t,[e])};var h=function(t){var n=jQuery.extend({},t);var r=this;if(r.hoverIntent_t){r.hoverIntent_t=clearTimeout(r.hoverIntent_t)}if(t.type=="mouseenter"){u=n.pageX;a=n.pageY;e(r).on("mousemove.hoverIntent",f);if(r.hoverIntent_s!=1){r.hoverIntent_t=setTimeout(function(){l(n,r)},i.interval)}}else{e(r).off("mousemove.hoverIntent",f);if(r.hoverIntent_s==1){r.hoverIntent_t=setTimeout(function(){c(n,r)},i.timeout)}}};return this.on({"mouseenter.hoverIntent":h,"mouseleave.hoverIntent":h},i.selector)}})(jQuery); 
 
-var l=0;
+var $gthis, l=0;
 var docType = /<\!DOCTYPE[^>]*>/i;
 var tagso = /<(html|head|body|title|meta|script|link)([\s\>])/gi;
 var tagsc = /<\/(html|head|body|title|meta|script|link)\>/gi;
@@ -583,25 +583,24 @@ function _init(opts) {
 }
 
 function _isInDivs(l) { 
-    var isInDiv = false;
-	$this.each(function () { 
-                try { if ($(l).parents("#" + $(this).attr("id")).length > 0) isInDiv = true; } catch(e) { alert(e); }
+    var isInDivs = false;
+        $this.each(function () { 
+                try { if ($(l).parents("#" + $(this).attr("id")).length > 0) isInDivs = true; } catch(e) { alert(e); }
             });
-     
-    return isInDiv;
+    
+    return isInDivs;
 }
 
 function _prefetch(e) { post = null;
      var link = e.currentTarget;
      if(window.location.protocol !== link.protocol || window.location.host !== link.host) return;
      
-     var req2 = function(){  
+     var req2 = function(){ 
          if(options.previewoff===true) return;
-         if(options.previewoff===false || (!_isInDivs(link) && !$(link).closest(options.previewoff).length)) _click(e, true);
+         if(!_isInDivs(link) && (options.previewoff===false || !$(link).closest(options.previewoff).length)) _click(e, true); 
      };
     	 
 	 $this.getPage('+', link.href, req2);
-     
 }
 
 function drain() {}
