c3acde792aa9782827d7f8ada8bb81f3f638dc87
Arvind Gupta <arvgta@gmail.com>
2015-04-27 13:24:08 +0200

    In response to #57 - interface commented in code
    Copied from http://4nf.org/interface/

--- ajaxify.js
+++ ajaxify.js
@@ -5,6 +5,53 @@
  * 
  * Copyright Arvind Gupta; MIT Licensed 
  */ 
+ 
+/* INTERFACE: See also http://4nf.org/interface/
+
+Options default values
+{
+// basic config parameters
+    selector : "a:not(.no-ajaxy)", //Selector for elements to ajaxify - without being swapped - e.g. a selection of links
+    forms : "form:not(.no-ajaxy)", // jQuery selection for ajaxifying forms - set to "false" to disable
+    canonical : true, // Fetch current URL from "canonical" link if given, updating the History API.  In case of a re-direct...
+ 
+// visual effects settings
+    requestDelay : 0, //in msec - Delay of Pronto request
+    aniTime : 0, //in msec - must be set for animations to work
+    aniParams : false, //Animation parameters - see below.  Default = off
+    previewoff : true, // Plugin previews prefetched pages - set to "false" to enable or provide a jQuery selection to selectively disable
+    idleTime: 0, //in msec - master switch for slideshow / carousel - default "off"
+    slideTime: 0, //in msec - time between slides
+    menu: false, //Selector for links in the menu
+    addclass: "jqhover", //Class that gets added dynamically to the highlighted element in the slideshow
+ 
+// script and style handling settings, prefetch
+    deltas : true, // true = deltas loaded, false = all scripts loaded
+    inline : true, // true = all inline scripts loaded, false = only specific inline scripts are loaded
+    inlinehints : false, // strings - separated by ", " - if matched in any inline scripts - only these are executed - set "inline" to false beforehand
+    inlineskip : "adsbygoogle", // strings - separated by ", " - if matched in any inline scripts - these are NOT are executed - set "inline" to true beforehand 
+    style : true, // true = all style tags in the head loaded, false = style tags on target page ignored
+    prefetch : true, // Plugin pre-fetches pages on hoverIntent or touchstart
+ 
+// debugging & advanced settings
+    verbosity : 0,  //Debugging level to console: 1 = medium, 2 = verbose
+    memoryoff : false, // strings - separated by ", " - if matched in any URLs - only these are NOT executed - set to "true" to disable memory completely
+    cb : null, // callback handler on completion of each Ajax request - default null
+    pluginon : true // Plugin set "on" or "off" (==false) manually
+}
+
+Animation parameters:  Default is false (set off)
+{
+    opacity: 1, //no fade, set to 0 for maximum fade
+    width: "100%", //in percent -  "100%" means no change
+    height: "100%" //in percent -  "100%" means no change
+}
+
+More animation parameters
+
+You can specify any parameters that are understood by .css() or .animate() respectively…
+
+*/
 
  //Intuitively better understandable shorthand for String.indexOf() - String.iO()
 String.prototype.iO = function(s) { return this.toString().indexOf(s) + 1; };
