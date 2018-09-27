51c384ffa48b176beb297e4ee38dae126dc10bdd
Arvind Gupta <arvgta@gmail.com>
2018-09-27 12:24:24 +0200

    Comment on external JS loading inserted

--- ajaxify.js
+++ ajaxify.js
@@ -44,7 +44,7 @@ Options default values
 // script and style handling settings, prefetch
     deltas : true, // true = deltas loaded, false = all scripts loaded
     asyncdef : false, // default async value for dynamically inserted external scripts, false = synchronous / true = asynchronous
-    alwayshints: "leaflet-embed", // strings, - separated by ", " - if matched in any external script URL - these are always loaded on every page load
+    alwayshints: false, // strings, - separated by ", " - if matched in any external script URL - these are always loaded on every page load
     inline : true, // true = all inline scripts loaded, false = only specific inline scripts are loaded
     inlinehints : false, // strings - separated by ", " - if matched in any inline scripts - only these are executed - set "inline" to false beforehand
     inlineskip : "adsbygoogle", // strings - separated by ", " - if matched in any inline scripts - these are NOT are executed - set "inline" to true beforehand 
@@ -444,7 +444,7 @@ pO("detScripts", { head: 0, lk: 0, j: 0 }, 0, function ($s) {
 // pk parameter:
 // href - operate on stylesheets in the new selection
 // src - operate on JS scripts
-pO("addAll", { $scriptsO: [], $sCssO: [], $sO: [], PK: 0 }, { deltas: true, asyncdef: false, alwayshints: "leaflet-embed" }, function ($this, pk) {
+pO("addAll", { $scriptsO: [], $sCssO: [], $sO: [], PK: 0 }, { deltas: true, asyncdef: false, alwayshints: false }, function ($this, pk) {
     if(!$this.length) return; //ensure input
     if(deltas === "n") return true; //If delta-loading disabled, return quickly
 	
@@ -509,9 +509,11 @@ pO("addAll", { $scriptsO: [], $sCssO: [], $sO: [], PK: 0 }, { deltas: true, asyn
 
 		if(PK == "href") return $(linki.replace("*", url)).appendTo("head"); //insert single stylesheet
 	    if(!url) return $.scripts($S); //insert single inline script
-
-        var script = document.createElement("script"), aSync = $S.attr("async"), deFer = $S.attr("defer");
-        script.type = "text/javascript";
+		
+        //Insert single external JS script - we have to go low level to avoid a warning coming from jQuery append()
+        //But we'll do our best to support all salient attributes
+		var script = document.createElement("script"), Type = $S.attr("type"), aSync = $S.attr("async"), deFer = $S.attr("defer");
+        if(Type) script.type = Type;
         script.src = url;
         script.async = aSync ? true : asyncdef;
         if(deFer) script.defer = true;
