b36fd47d4f0e8a99a6bc4b254ae8a1ac7e91393c
Arvind Gupta <arvgta@gmail.com>
2018-10-24 16:49:04 +0200

    Working draft for #156 - external JS inclusion

--- ajaxify.js
+++ ajaxify.js
@@ -512,12 +512,16 @@ pO("addAll", { $scriptsO: [], $sCssO: [], $sO: [], PK: 0 }, { deltas: true, asyn
 		
         //Insert single external JS script - we have to go low level to avoid a warning coming from jQuery append()
         //But we'll do our best to support all salient attributes
-		var script = document.createElement("script"), Type = $S.attr("type"), aSync = $S.attr("async"), deFer = $S.attr("defer");
-        if(Type) script.type = Type;
-        script.src = url;
-        script.async = aSync ? true : asyncdef;
-        if(deFer) script.defer = true;
-        document.head.appendChild(script);
+        var script = document.createElement("script");
+        _cloneAttributes(script, $S); //clone all attributes of script element generically
+        document.head.appendChild(script); //it doesn't matter much, if we append to head or content div
+    },
+    cloneAttributes: function (el, $S) { //clone all attributes of script element generically
+        var attr, attributes = Array.prototype.slice.call($S[0].attributes); //slice performs a copy, too
+        el.async = asyncdef; //initialise with asyncdef - may be overwritten in the while loop
+        while(attr = attributes.pop()) { //fetch one of all the attributes at a time
+            el.setAttribute(attr.nodeName, attr.nodeValue); //low-level insertion
+        }
     },
     findScript: function (url) { //Find URL in old array
         if(!url) return false;
