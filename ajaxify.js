f8c9dc7dff985c4f36a73fd33289cb46a44af1de
Arvind Gupta <arvgta@gmail.com>
2019-05-27 20:35:26 +0200

    Superfluous try + catch deleted

--- ajaxify.js
+++ ajaxify.js
@@ -118,12 +118,7 @@ function _copyAttributes(el, $S, flush) { //copy all attributes of element gener
     if (flush) //delete all old attributes
         for (var i = el.attributes.length - 1; i >= 0; i--) el.removeAttribute(el.attributes[i].name);
 
-    var attr, attributes;
-    try { //in case of bad input data, $S may contain garbage or nothing
-        attributes = Array.prototype.slice.call($S[0].attributes); //slice performs a copy, too
-    } catch(e) {
-        $.log("Bad input data");
-    }
+    var attr, attributes = Array.prototype.slice.call($S[0].attributes); //slice performs a copy, too;
 	
     while (attr = attributes.pop()) { //fetch one of all the attributes at a time
         el.setAttribute(attr.nodeName, attr.nodeValue); //low-level insertion
