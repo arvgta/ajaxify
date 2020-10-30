7a38e9be5ad8956c4e436f055c62ee9b5fb5ca26
Arvind Gupta <arvgta@gmail.com>
2020-10-30 14:31:16 +0100

    Going ES6 - decoupled from $.hApi()

--- ajaxify.js
+++ ajaxify.js
@@ -84,7 +84,7 @@ scrr = 'script[src*="!"]',
 inlineclass = "ajy-inline";
 
 //Module global classes
-let pages, memory, cache1, getPage, fn, scripts, detScripts, addAll, Rq, frms, offsets, scrolly;
+let pages, memory, cache1, getPage, fn, scripts, detScripts, addAll, Rq, frms, offsets, scrolly, hApi;
 
 //Minified pO() function - for documentation of pO() please refer to https://4nf.org/po/
 var funStr,logging=!1,codedump=!1;let getParamNames=()=>funStr.slice(funStr.indexOf("(")+1,funStr.indexOf(")"));function JSON2Str(n,t){let e="";return Object.entries(n).forEach(([n,o],r)=>{e+=`${r?",\n":""}`+("function"==typeof o?`_${n} = ${iLog(o.toString(),n)}`:`${n} = ${t?'settings["':""}${t?n+'"]':JSON.stringify(o)}`)}),e?`let ${e}${0!=t?";":""}`:""}function pO(n,t,e,o,r,s){let i,l,u,g,f,$,c,a,p="",d="",O="";if(!n||!o)return console.log("Error in pO(): Missing parameter");if(funStr=iLog(funStr=o.toString(),n),i=n.substr(0,1).toUpperCase()+n.substr(1,n.length-1),g=(l=getParamNames(o)).indexOf("$this")+1,f=l.indexOf("options")+1,u=l.replace("$this, ",""),u="$this"==l?"":u,e&&!f&&(u+=""===u?"options":", options"),t&&(p=JSON2Str(t)),e&&(d=`let settings = $.extend(${JSON.stringify(e)}, options);\n${JSON2Str(e,1)}`),r&&(O=JSON2Str(r,0)),a=`\n(function ($) { class ${i} {\n        constructor(${$=e?"options":""}) {\n            ${p}\n            ${d}\n            this.a = ${funStr};\n            ${O}\n        }\n    }\n\n    $.${c=g?"fn."+n:n} = function(${u}) {${g?"let $this = $(this);":""}\n        if(!$.${c}.o) $.${c}.o = new ${i}(${$});\n        return $.${c}.o.a(${l});\n    };\n})(jQuery);`,1!=codedump&&codedump!==i.toLowerCase()||console.log(a),!s)try{jQuery.globalEval(a)}catch(n){console.log(`Error: ${n} | ${a}`)}}function showArgs(n){s="";for(var t=0;t<n.length;t++)null==n[t]?s+="null | ":s+=(null!=n[t]&&"function"!=typeof n[t]&&"object"!=typeof n[t]&&("string"!=typeof n[t]||n[t].length<=100)?n[t]:"string"==typeof n[t]?n[t].substr(0,100):typeof n[t])+" | ";return s}function iLog(n,t){if(n=n.replace(/\/\*[\s\S]*?\*\/|\/\/.*/g,""),!logging||"log"===t)return n;let e=n.indexOf("=>")<30?n.indexOf("=>")+1:0,o=n.indexOf("{")+1;e&&(n=n.replace(/(	|\r\n|\n|\r)/gm,""),(!o||o>e+5)&&(n=`${n.substr(0,e+2)}{ return ${n.substr(e+1)}}`),n="function ("+n.substr(0,n.indexOf("{")-3).trim().replace(/\(/g,"").replace(/\)/g,"")+")"+n.substr(n.indexOf("{")).trim()),o=n.indexOf("{");let r=n.substr(n.indexOf("("),n.indexOf(")")-n.indexOf("(")+1).replace(/"/g,'\\"').replace(/'/g,"\\'");return`${n.substr(0,o)}{$.log(lvl + " | ${t} | ${r} | " + showArgs(arguments)${2==logging?", -1, true, arguments":""}); try { lvl++; ${n.substr(o+1,n.length-o-2)}} finally {lvl--;}}`}pO("log",0,{verbosity:0},function(n,t,e,o){if(t>=0&&(verbosity=t),verbosity&&n&&lvl<=verbosity&&console&&1==e)return console.groupCollapsed(n),console.table(o),console.groupCollapsed("Trace"),console.trace(),console.groupEnd(),void console.groupEnd();verbosity&&n&&lvl<=verbosity&&console&&console.log(n)});
@@ -711,13 +711,16 @@ let _scrll = o => jQuery(window).scrollTop(o)
 // Switch (o) values:
 // = - perform a replaceState, using currentURL
 // otherwise - perform a pushState, using currentURL
-pO("hApi", 0, 0, function (o, p) {
-	if(!o) return; //ensure operator
-	if(p) currentURL = p; //if p given -> update current URL
+class classHApi { constructor() {
+            
+	this.a = function (o, p) {
+		if(!o) return; //ensure operator
+		if(p) currentURL = p; //if p given -> update current URL
 
-	if(o === "=") history.replaceState({ url: currentURL }, "state-" + currentURL, currentURL); //perform replaceState
-	else if (currentURL !== window.location.href) history.pushState({ url: currentURL }, "state-" + currentURL, currentURL); //perform pushState
-});
+		if(o === "=") history.replaceState({ url: currentURL }, "state-" + currentURL, currentURL); //perform replaceState
+		else if (currentURL !== window.location.href) history.pushState({ url: currentURL }, "state-" + currentURL, currentURL); //perform pushState
+	};
+}}
 
 // The Pronto plugin - Pronto variant of Ben Plum's Pronto plugin - low level event handling in general
 // Works on a selection, passed to Pronto by the selection, which specifies, which elements to Ajaxify
@@ -738,6 +741,7 @@ pO("pronto", { $gthis: 0, requestTimer: 0, pfohints: 0, pvohints: 0 }, { selecto
 		if($.slides) $.slides(0, s); //initialise optional slideshow sub-plugin
 		scrolly = new classScrolly(); //initialise scroll effects sub-plugin
 		offsets = new classOffsets();
+		hApi = new classHApi();
 		_init_p(); //initialise Pronto sub-plugin
 		return $this; //return jQuery selector for chaining
 	}
@@ -754,7 +758,7 @@ pO("pronto", { $gthis: 0, requestTimer: 0, pfohints: 0, pvohints: 0 }, { selecto
 	}
 }, { 
 	init_p: () => {
-		$.hApi("=", window.location.href); // Set initial state
+		hApi.a("=", window.location.href); // Set initial state
 		$(window).on("popstate", _onPop); // Set handler for popState
 		if (prefetchoff !== true) {
 			$(document).hoverIntent(_prefetch, () => {}, selector); //this type of call also handles dynamically inserted links
@@ -797,13 +801,13 @@ pO("pronto", { $gthis: 0, requestTimer: 0, pfohints: 0, pvohints: 0 }, { selecto
 		if(!href || _exoticKey()) return; // Ignore everything but normal click
 		if(href.substr(-1) ==="#") return true;
 		if(_hashChange()) { // only hash part has changed
-			$.hApi("=", href); // commit new URL to History API
+			hApi.a("=", href); // commit new URL to History API
 			return true; // Enable default behaviour and return - does not invoke a full page load!
 		}
 
 		scrolly.a("+"); // Capture old vertical position of scroll bar
 		_stopBubbling(e); // preventDefault and stop bubbling-up from here on, no matter what comes next
-		if(Rq.a("=")) $.hApi("="); // if new URL is same as old URL, commit to History API
+		if(Rq.a("=")) hApi.a("="); // if new URL is same as old URL, commit to History API
 		if(refresh || !Rq.a("=")) _request(notPush); // Continue with _request() when not the same URL or "refresh" parameter set hard
 	}, 
 	request: notPush => { // ... new url
@@ -845,7 +849,7 @@ pO("pronto", { $gthis: 0, requestTimer: 0, pfohints: 0, pvohints: 0 }, { selecto
 		var href = Rq.a("h"), // Retrieve href 
 		href = Rq.a("c", href); // Fetch canonical if no hash or parameters in URL
 
-		$.hApi(Rq.a("p") ? "+" : "=", href); // Push new state to the stack on new url
+		hApi.a(Rq.a("p") ? "+" : "=", href); // Push new state to the stack on new url
 		if (fn.a("title")) $("title").html(fn.a("title").html()); // Update title
 		Rq.a("C", fn.a("-", $gthis)); // Update DOM and fetch canonical URL
 		frms.a("a"); // Ajaxify forms - in content divs only
